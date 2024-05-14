fd8cc6f85ce4d9a64b8a7331e3d3541acbd26a47
Taher Fawaz <67816257+taher-fawaz@users.noreply.github.com>
2024-05-14 21:46:13 +0300

    static func

--- lib/compressor.dart
+++ lib/compressor.dart
@@ -1,7 +1,7 @@
 import 'compressor_platform_interface.dart';
 
 class Compressor {
-  Future<String?> compressVideo(String inputPath, String outputPath) {
+  static Future<String?> compressVideo(String inputPath, String outputPath) {
     return CompressorPlatform.instance.compressVideo(inputPath, outputPath);
   }
 }
